5d81ae7b5cdf5876d61961639bf135bd85ac12cb
Gornostay-chik <79249847+Gornostay-chik@users.noreply.github.com>
2023-04-25 16:52:50 +0300

    Update main.js

--- tests/main.js
+++ tests/main.js
@@ -1,15 +1,25 @@
 // tests/main.js
 //import http from "k6/http";
-import { sleep } from "k6";
-import create_pet from "./create_pet.js";
-import get_pet from "./get_pet.js";
+import { sleep } from "k6"; // подключаем сон
+import create_pet from "./create_pet.js"; // подключаем создание питомца
+import get_pet from "./get_pet.js"; // подключет просмотр питомца
+import { group } from 'k6'; // подключем группировку тестов - !!! переменные не живут между группами
 
-const apiEndpoint = `https://petstore.swagger.io/v2/pet/`;
+
+const apiEndpoint = `https://petstore.swagger.io/v2/pet/`; // адрес обращения
+
 
 export default function () {
 // console.log("Hello from K6");
 // sleep(1);
-var pet_id = create_pet(apiEndpoint);
+group('API create and get', () => {
+var pet_id = create_pet(apiEndpoint); // переменная живет внутри группы
+//});
+
  sleep(1);
+
+// group('API get', () => {
  get_pet(apiEndpoint,pet_id);
+});
+
 }
