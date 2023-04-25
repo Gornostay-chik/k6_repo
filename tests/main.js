5e51b2614a9539b2cac37b33272d13e2a399b37b
Gornostay-chik <79249847+Gornostay-chik@users.noreply.github.com>
2023-04-25 11:15:01 +0300

    Update main.js

--- tests/main.js
+++ tests/main.js
@@ -1,9 +1,15 @@
 // tests/main.js
 //import http from "k6/http";
 import { sleep } from "k6";
-import not_main from "./not_main.js";
+import create_pet from "./create_pet.js";
+import get_pet from "./get_pet.js";
+
+const apiEndpoint = `https://petstore.swagger.io/v2/pet/`;
+
 export default function () {
- console.log("Hello from K6");
+// console.log("Hello from K6");
+// sleep(1);
+var pet_id = create_pet(apiEndpoint);
  sleep(1);
- not_main();
+ get_pet(apiEndpoint,pet_id);
 }
