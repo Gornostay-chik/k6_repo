ab0a825e5b7fa8aed8662e03a8947e299a5ef853
Gornostay-chik <79249847+Gornostay-chik@users.noreply.github.com>
2023-04-25 11:15:39 +0300

    Delete not_main.js

--- tests/not_main.js
+++
@@ -1,7 +0,0 @@
-// tests/main.js
-import http from "k6/http";
-const apiEndpoint = `https://petstore.swagger.io/v2/pet/`;
-export default function () {
- // console.log("Hello from K6");
- http.get(`${apiEndpoint}9223372036854775095`);
-}
