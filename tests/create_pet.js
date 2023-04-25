7981d18884a55bb7bf73a8bad29d9f199639eab2
Gornostay-chik <79249847+Gornostay-chik@users.noreply.github.com>
2023-04-25 16:51:39 +0300

    Update create_pet.js

--- tests/create_pet.js
+++ tests/create_pet.js
@@ -1,5 +1,9 @@
 // tests/main.js
 import http from "k6/http";
+import { check} from 'k6'; // подключаем проверки - тест не прерывается в  случае неудачи !!
+import { Trend } from 'k6/metrics'; // подключаем возможность собственных метрик
+
+const TrendCheckCR = new Trend('/API create');
 
 export default function (apiEndpoint) {
  // console.log("Hello from K6");
@@ -30,6 +34,18 @@ export default function (apiEndpoint) {
  console.log("create_pet "+randomId);
  console.log(res.json());
 
+ TrendCheckCR.add(res.timings.duration);
+
+ // блок проверок
+ check(res, {
+  "create status code should be 200": res => res.status === 200,
+});
+
+  check(res, {
+    "create response should have status available": res => res.json().status === "available",
+});
+
+// возврат переменной для следующего скрипта
   return randomId;
 
 }
